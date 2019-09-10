4c239ffcdb6bea950775a90338598730ae10df8a
Sam Kelly <gotosam1212@gmail.com>
2019-09-10 20:28:00 +1000

    added a closer to real image and the zoom/pan functionality for T also added "react-responsive-pinch-zoom-pan" as a dependancy

--- src/T.js
+++ src/T.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {Link} from 'react-router-dom';
+import PinchZoomPan from "react-responsive-pinch-zoom-pan";
 
 
 class T extends React.Component {
@@ -10,7 +11,13 @@ class T extends React.Component {
 	render() {
 		return (
 			<div>
-				<div><img src={ require('./images/test3.jpg') } /></div>
+				<h1>T</h1>
+				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+				<div style={{ width: '500px', height: '500px' }}>
+<PinchZoomPan>
+<img src={ require('./images/Small_intestine_low_mag.jpg') } />
+</PinchZoomPan>
+				</div>
 				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
 				<div className="buttons">
 					<Link to="/intro"> <button type="button">BACK</button> </Link>
