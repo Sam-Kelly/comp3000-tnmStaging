5ab1c38087c5800f3e0953f6113be565e63494b9
amraj_singh <amrajsingh957@gmail.com>
2019-09-23 21:58:38 +1000

    Layout Optimisation attempts, changed positioning, size and organisation of elements across T stage page

--- src/T.js
+++ src/T.js
@@ -9,29 +9,42 @@ class T extends React.Component {
 
 	render() {
 		return (
-			<div>			
-				<h1>Tumour Stage (T)</h1>
-				<p className="col-sm-12">The following diagram shows an example of a biopsy from a patient. Based on the areas highlighted, select the stage that you think best represents the tumour's current stage.	
-				</p>
-				<div className="col-sm-8" style={{ background:"grey", padding: "5px", maxHeight:"500px", maxWidth:"100%", display:"inline-block"}}>
+			<div>
+				<h1>TNM Staging </h1>		
+				<div className="nav">
+						<Link to="/intro"> <button type="button" className="col-sm-3">Intro</button> </Link>
+						<Link to="/t"> <button type="button" className="col-sm-3">Tumour Stage (T)</button> </Link>
+						<Link to="/n"> <button type="button" className="col-sm-3">Nodes Stage (N)</button> </Link>
+						<Link to="/m"> <button type="button" className="col-sm-3">Metastasis Stage (M)</button> </Link>
+				</div>
+
+				<h3>Tumour Stage (T)</h3>
+				<p className="col-sm-12">The histopathology for Patricia’s cancer is shown below. What is the value of “T”?</p>
+				
+				<div className="sidebar col-sm-12" style={{padding: "5px", maxHeight:"500px", maxWidth:"100%"}}>
+					<div>
+						<span className="col-sm-2"></span>
+							<button className="col-sm-3" id="stageI">Stage I</button>
+						<span className="col-sm-2"></span>
+							<button className="col-sm-3" id="stageII">Stage II</button>
+						<span className="col-sm-2"></span>
+					</div><br></br><br></br>
+					<div>
+						<span className="col-sm-2"></span>
+							<button className="col-sm-3" id="stageIII">Stage III</button>
+						<span className="col-sm-2"></span>
+							<button className="col-sm-3" id="stageIV">Stage IV</button>
+						<span className="col-sm-2"></span><br></br>
+					</div>
+				</div>
+
+				<div className="col-sm-12" style={{ background:"grey", padding: "5px", maxWidth:"100%", display:"inline-block"}}>
 				
 				<PinchZoomPan>
-					<img src={ require('./images/T_cancer.jpg') } />
+					<img style={{ background:"grey"}} src={ require('./images/T_cancer.jpg') } />
 				</PinchZoomPan>
 
-				</div>
-				<div className="sidebar col-sm-4" style={{ background:"red", padding: "5px", 
-				border: "5px solid black", minHeight:"500px", maxHeight:"500px", maxWidth:"100%"}}>
 
-					<p>Select stage:</p><br></br><br></br>
-					<button onClick="onCorrectClick" id="stageI">Stage I</button><br></br><br></br>
-					<button id="stageII">Stage II</button><br></br><br></br>
-					<button id="stageIII">Stage III</button><br></br><br></br>
-					<button id="stageIV">Stage IV</button><br></br><br></br><br></br><br></br>
-
-					<Link to="/intro"> <button style={{ padding: "5px", fontSize: "15px", textDecoration: "none",
-					 color: "black", float: "none", border:"2px solid black"}} type="button">BACK</button> </Link>
-				
 				
 				</div>
 			</div>
