2e2e6b5718589b996288ba8c661749b61757d667
amraj_singh <amrajsingh957@gmail.com>
2019-09-26 20:26:05 +1000

    Consistency improvements to N from master merge

--- src/N.js
+++ src/N.js
@@ -5,13 +5,37 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {Link} from 'react-router-dom';
 
 class N extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {nodesSelected: 0, answer: <p></p>};
+		this.handleClick = this.handleClick.bind(this);
+		this.checkAnswer = this.checkAnswer.bind(this);
+	}
+
+	handleClick(value) {
+		this.setState({
+			nodesSelected: this.state.nodesSelected += value
+		})
+		console.log(this.state.nodesSelected)
+	}
+
+	checkAnswer() {
+		var elem;
+		if (this.state.nodesSelected >= 1 && this.state.nodesSelected <= 3) {
+			elem = <div style={{border:"2px solid green", margin:"2px", padding: "15px", clear:"left"}}>Correct! N1 is 1 - 3 regional lymph nodes.</div>
+		} else {
+			elem = <div style={{border:"2px solid red", margin:"2px", padding: "15px", clear:"left"}}>Incorrect! N1 is 1 - 3 regional lymph nodes.</div>
+		}
+		this.setState({
+			answer: elem
+		})
+		
+	}
 
 	render() {
 		return (
 			<div>
-			
-			<h1>TNM Staging </h1>		
-				<div className="nav">
+			<div className="nav">
 						<Link to="/intro"> <button type="button" className="col-sm-3">Intro</button> </Link>
 						<Link to="/t"> <button type="button" className="col-sm-3">Tumour Stage (T)</button> </Link>
 						<Link to="/n"> <button type="button" className="col-sm-3">Nodes Stage (N)</button> </Link>
@@ -21,21 +45,24 @@ class N extends React.Component {
 				<h3>Node Stage (N)</h3>
 				<p className="col-sm-12">It was found that the value for “N” is 1. Please select the correct amount of lymph nodes that corresponds to N1 in the diagram below. </p>
 				
-				<div id="n-id col-sm-6">
-					<img id="n-img" src={ require('./images/n.gif') } alt="lymph node image" /> 
-					<svg id="n-svg" width="470" height="543">
-					<NCircle id="n1" cx="163" cy="270" r="5" />
-					<NCircle id="n2" cx="152" cy="272" r="5" />
-					<NCircle id="n3" cx="140" cy="285" r="6" />
-					<NCircle id="n4" cx="141" cy="307" r="5" />
-					<NCircle id="n5" cx="142" cy="318" r="5" />
-					<NCircle id="n6" cx="152" cy="314" r="5" />
-					<NCircle id="n7" cx="146" cy="340" r="6" />
-					<NCircle id="n8" cx="140" cy="356" r="6" />
-				</svg>
-				</div>
 
-				<button className="col-sm-4" id="Submit">Submit</button>
+			<div id="n-id">
+				<img id="n-img" src={ require('./images/n.gif') } alt="lymph node image" /> 
+				<svg id="n-svg" width="470" height="543">
+				<NCircle handler={this.handleClick} id="n1" cx="163" cy="270" r="5" />
+				<NCircle handler={this.handleClick} id="n2" cx="152" cy="272" r="5" />
+				<NCircle handler={this.handleClick} id="n3" cx="140" cy="285" r="6" />
+				<NCircle handler={this.handleClick} id="n4" cx="141" cy="307" r="5" />
+				<NCircle handler={this.handleClick} id="n5" cx="142" cy="318" r="5" />
+				<NCircle handler={this.handleClick} id="n6" cx="152" cy="314" r="5" />
+				<NCircle handler={this.handleClick} id="n7" cx="146" cy="340" r="6" />
+				<NCircle handler={this.handleClick} id="n8" cx="140" cy="356" r="6" />
+				</svg>
+			</div>
+			<button className="col-sm-4" onClick={this.checkAnswer}>
+				Submit answer
+			</button>
+			{this.state.answer}
 			</div>
 		); 
 	}
@@ -53,8 +80,10 @@ class NCircle extends React.Component {
 		newToggleOnValue = !this.state.isToggleOn;
 		if(this.state.colour == "transparent") {
 			newColourValue = "red"
+			this.props.handler(1);
 		} else {
 			newColourValue = "transparent"
+			this.props.handler(-1);
 		}
 		this.setState({
 			isToggleOn: newToggleOnValue,
