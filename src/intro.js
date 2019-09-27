cb80341b30128a4fd4923bfb9b2bbad58d9ad960
Sam Kelly <gotosam1212@gmail.com>
2019-09-27 11:30:00 +1000

    why wont publishing this stuff work?

--- src/intro.js
+++ src/intro.js
@@ -15,7 +15,7 @@ class Intro extends React.Component {
 			return (
 				<div>
 					<div><img src={ require('./images/test.jpg') } /></div>
-					<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+					<p> Change to test some stuff. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
 					<div className="buttons">
 						<Link to="/t"> <button type="button">T</button> </Link>
 						<Link to="/n"> <button type="button">N</button> </Link>
