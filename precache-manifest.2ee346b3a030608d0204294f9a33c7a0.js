b0204ad40ad9f5d0795927dfa0be989aab61bf65
Sam Kelly <gotosam1212@gmail.com>
2019-09-27 10:35:18 +1000

    Updates

---
+++ precache-manifest.2ee346b3a030608d0204294f9a33c7a0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e497a0ce57c97164771b8a699631a01",
+    "url": "/comp3000-tnmStaging/index.html"
+  },
+  {
+    "revision": "cdb31e7bf373556f7062",
+    "url": "/comp3000-tnmStaging/static/css/2.fb6ed098.chunk.css"
+  },
+  {
+    "revision": "61c2a014c80a62031336",
+    "url": "/comp3000-tnmStaging/static/css/main.a3e56332.chunk.css"
+  },
+  {
+    "revision": "cdb31e7bf373556f7062",
+    "url": "/comp3000-tnmStaging/static/js/2.0b6d60d3.chunk.js"
+  },
+  {
+    "revision": "61c2a014c80a62031336",
+    "url": "/comp3000-tnmStaging/static/js/main.b8cac494.chunk.js"
+  },
+  {
+    "revision": "393d2aebad5454c15a97",
+    "url": "/comp3000-tnmStaging/static/js/runtime~main.89fab912.js"
+  },
+  {
+    "revision": "79be7f990ff324681e168b233e2bd07a",
+    "url": "/comp3000-tnmStaging/static/media/T_cancer.79be7f99.jpg"
+  },
+  {
+    "revision": "85c01dd35e8f442bfa5f36e20ba6714c",
+    "url": "/comp3000-tnmStaging/static/media/mucosa.85c01dd3.png"
+  },
+  {
+    "revision": "d18111dc52f181bc45dcfe3fefc29db3",
+    "url": "/comp3000-tnmStaging/static/media/muscularis.d18111dc.png"
+  },
+  {
+    "revision": "41bfa8f69b0c651c6455a7350fb736ee",
+    "url": "/comp3000-tnmStaging/static/media/n.41bfa8f6.gif"
+  },
+  {
+    "revision": "66a91a21014a94fa5a6153df5003d871",
+    "url": "/comp3000-tnmStaging/static/media/serosa.66a91a21.png"
+  },
+  {
+    "revision": "8a88a5418715b495b0422aa1eb0485a9",
+    "url": "/comp3000-tnmStaging/static/media/submucosa.8a88a541.png"
+  },
+  {
+    "revision": "7e9be033a955f18f777a87d28f93be3e",
+    "url": "/comp3000-tnmStaging/static/media/test.7e9be033.jpg"
+  },
+  {
+    "revision": "9890c117374d4e9618a86e6cc621c839",
+    "url": "/comp3000-tnmStaging/static/media/test6.9890c117.jpg"
+  }
+]);
